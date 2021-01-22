a986df25ed94cf373df863abba29e6a44229495e
Bede Marcos <bedesahlu@gmail.com>
2021-01-21 19:12:30 -0500

    Add unstyled color slider

--- colors-app/src/App.js
+++ colors-app/src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 import Palette from "./Palette";
+import { generatePalette } from "./color-helpers";
 import seedColors from "./seedColors";
 
 class App extends Component {
   render() {
+    // console.log(generatePalette(seedColors[4]));
     return (
       <div className="App">
-        <Palette {...seedColors[2]} />
+        <Palette palette={generatePalette(seedColors[4])} />
       </div>
     );
   }
