3d8f449a5d7f10330fada58f2c1619f218508048
Bede Marcos <bedesahlu@gmail.com>
2021-01-26 23:29:37 -0500

    Start styling MiniPalette

--- colors-app/src/MiniPalette.js
+++ colors-app/src/MiniPalette.js
@@ -1,32 +1,53 @@
 import React, { Component } from "react";
 import { withStyles } from "@material-ui/styles";
 
-
 ///JSS also known as CSS in JS. This feature
-// allows us to use nestings, and component 
+// allows us to use nestings, and component
 //specific styling. Syntax for it is still: JS (Camelcase and Strings).
 const styles = {
-  main: {
-    backgroundColor: "purple",
-    border: "3px solid teal",
-    "& h1": {
-        backgroundColor: "orange",
-        display: "inline-block"
-    }
+  root: {
+    backgroundColor: "white",
+    borderRadius: "5px",
+    padding: "0.5rem",
+    position: "relative",
+    overflow: "hidder",
+    border: "1px solid black",
+    "&:hover": {
+      cursor: "pointer",
+    },
   },
-
+  colors: {
+    backgroundColor: "grey",
+  },
+  title: {
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    margin: 0,
+    color: "black",
+    paddingTop: "0.5rem",
+    fontSize: "1rem",
+    position: "relative",
+  },
+  emoji: {
+    marginLeft: "0.5rem",
+    fontSize: "1.5rem",
+  },
 };
 
 class MiniPalette extends Component {
   render() {
     // we automatically get "classes" with our props
     //with material-ui. it takes our "main" style and
-    //gives it a unique id (on top of the class name) 
+    //gives it a unique id (on top of the class name)
     //which makes it unique to this component.
-    const { classes } = this.props;
+    const { classes, paletteName, emoji } = this.props;
     return (
-      <div className={classes.main}>
-        <h1>Mini Palette</h1>
+      <div className={classes.root}>
+        <div className={classes.colors}></div>
+        <h5 className={classes.title}>
+          {paletteName} <span className={classes.emoji}>{emoji}</span>
+        </h5>
       </div>
     );
   }
