ebbd16f878db878b773c5a665d49b3b79bc45144
Bede Marcos <bedesahlu@gmail.com>
2021-02-04 00:57:20 -0500

    Add Random Color and Clear Palette

--- colors-app/src/NewPaletteForm.js
+++ colors-app/src/NewPaletteForm.js
@@ -78,12 +78,15 @@ const styles = (theme) => ({
 });
 
 class NewPaletteForm extends React.Component {
+  static defaultProps = {
+    maxColors: 20,
+  };
   constructor(props) {
     super(props);
     this.state = {
       open: false,
       currentColor: "teal",
-      colors: [],
+      colors: this.props.palettes[0].colors,
       newColorName: "",
       newPaletteName: "",
     };
@@ -92,6 +95,8 @@ class NewPaletteForm extends React.Component {
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
     this.removeColor = this.removeColor.bind(this);
+    this.clearColors = this.clearColors.bind(this);
+    this.addRandomColor = this.addRandomColor.bind(this);
   }
 
   componentDidMount() {
@@ -135,9 +140,9 @@ class NewPaletteForm extends React.Component {
     });
   }
 
-  onSortEnd = ({oldIndex, newIndex}) => {
-    this.setState(({colors}) => ({
-      colors:arrayMove(colors, oldIndex, newIndex),
+  onSortEnd = ({ oldIndex, newIndex }) => {
+    this.setState(({ colors }) => ({
+      colors: arrayMove(colors, oldIndex, newIndex),
     }));
   };
 
@@ -157,9 +162,22 @@ class NewPaletteForm extends React.Component {
     this.props.history.push("/");
   }
 
+  clearColors() {
+    this.setState({ colors: [] });
+  }
+
+  addRandomColor() {
+    //pick random color from existing palettes
+    const allColors = this.props.palettes.map((p) => p.colors).flat();
+    let rand = Math.floor(Math.random() * allColors.length);
+    const randomColor = allColors[rand];
+    this.setState({ colors: [...this.state.colors, randomColor] });
+  }
+
   render() {
-    const { classes } = this.props;
+    const { classes, maxColors } = this.props;
     const { open } = this.state;
+    const paletteIsFull = this.state.colors.length >= maxColors;
 
     return (
       <div className={classes.root}>
@@ -225,10 +243,19 @@ class NewPaletteForm extends React.Component {
           <Divider />
           <Typography variant="h4">Design Your Palette</Typography>
           <div>
-            <Button variant="contained" color="secondary">
+            <Button
+              variant="contained"
+              color="secondary"
+              onClick={this.clearColors}
+            >
               Clear Palette
             </Button>
-            <Button variant="contained" color="primary">
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={this.addRandomColor}
+              disabled={paletteIsFull}
+            >
               Random Color
             </Button>
           </div>
@@ -249,12 +276,17 @@ class NewPaletteForm extends React.Component {
               ]}
             />
             <Button
+              disabled={paletteIsFull}
               variant="contained"
               color="primary"
-              style={{ backgroundColor: this.state.currentColor }}
+              style={{
+                backgroundColor: paletteIsFull
+                  ? "grey"
+                  : this.state.currentColor,
+              }}
               type="submit"
             >
-              Add Color
+              {paletteIsFull ? "Palette Is Full" : "Add Color"}
             </Button>
           </ValidatorForm>
         </Drawer>
@@ -267,7 +299,7 @@ class NewPaletteForm extends React.Component {
           <DraggleColorList
             colors={this.state.colors}
             removeColor={this.removeColor}
-            axis='xy'
+            axis="xy"
             onSortEnd={this.onSortEnd}
           />
           {/* {this.state.colors.map((color) => (
